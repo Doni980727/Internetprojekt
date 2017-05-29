7806125cd1bdb7e881fb2b2c1998700ec2c4c0cc
xDoni <dinof98@gmail.com>
2017-05-29 23:11:39 +0200

    s

---
+++ Uppgift/src/reader.java
@@ -0,0 +1,28 @@
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by Fedi14 on 2017-02-10.
+ */
+public class reader {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Skriv in önskade url: ");
+        Scanner tgb = new Scanner(System.in);
+        String url = tgb.nextLine();
+        URL website = new URL("https://" +url);
+        Scanner in = new Scanner(website.openStream());
+
+        int tolength = 0;
+
+
+
+        String rad;
+        while (in.hasNext()) {
+            rad = in.nextLine();
+            tolength += rad.length();
+        }
+
+        System.out.println("Totala tecken i koden är " + tolength);
+    }
+}
